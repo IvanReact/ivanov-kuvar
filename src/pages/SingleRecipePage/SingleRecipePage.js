4b3353bf6bc698508c02b59c3ee1e2980aadc5f4
Ivan <susnicaivan90@gmail.com>
2022-05-23 11:07:16 +0200

    hooks import changes

--- src/pages/SingleRecipePage/SingleRecipePage.js
+++ src/pages/SingleRecipePage/SingleRecipePage.js
@@ -1,6 +1,5 @@
 import { React, useContext, useState } from "react";
-import { useParams, Redirect } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 import "./style.scss";
 
